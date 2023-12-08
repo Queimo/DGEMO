ac0c38bc5fd919e9b821cf561e35c3a4f92a544c
Karim Ben Hicham <queimo@gmx.net>
2023-12-08 23:22:30 +0800

    parallel mode

--- main.sh
+++ main.sh
@@ -6,21 +6,21 @@
 
 
 ### File / path where STDOUT will be written, %J is the job id
-#SBATCH --output=training-out_mobo.%J
+#SBATCH --output=optimization-out_mobo.%J
 
 ### Request the time you need for execution. The full format is D-HH:MM:SS
 ### You must at least specify minutes or days and hours and may add or
 ### leave out any other parameters
-#SBATCH --time=08:00:00
+#SBATCH --time=16:00:00
 
 ### Request memory you need for your job in MB
-#SBATCH --mem-per-cpu=4000
+#SBATCH --mem-per-cpu=2000
 ### Request number of CPUs
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=24
 
 ### Change to the work directory
 cd $HPCWORK/DGEMO
 source $HOME/.zshrc
 micromamba activate mobo_replica
 
-python ./main.py $@
+python ./scripts/run.py $@
