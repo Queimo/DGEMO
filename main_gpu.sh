5d6a62d5b01523667190706927aa3de4a352f662
Karim Ben Hicham <karim.ben.hicham@rwth-aachen.de>
2024-01-16 19:31:35 +0800

    add gpu

---
+++ main_gpu.sh
@@ -0,0 +1,27 @@
+#!/usr/local_rwth/bin/zsh
+
+### Job name
+#SBATCH --job-name=mobo
+#### SBATCH --account=rwth1430
+
+
+### File / path where STDOUT will be written, %J is the job id
+#SBATCH --output=optimization-out_mobo.%J
+
+### Request the time you need for execution. The full format is D-HH:MM:SS
+### You must at least specify minutes or days and hours and may add or
+### leave out any other parameters
+#SBATCH --time=2:00:00
+
+### Request memory you need for your job in MB
+#SBATCH --mem-per-cpu=2000
+### Request number of CPUs
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+
+### Change to the work directory
+cd $HPCWORK/DGEMO
+source $HOME/.zshrc
+micromamba activate mobo
+
+python ./run.py $@
