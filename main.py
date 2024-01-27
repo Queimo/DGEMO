f2c3d8464452c2c3cd267983456a6a3e7c3c8128
Karim Ben Hicham <karim.ben.hicham@rwth-aachen.de>
2024-01-27 22:31:51 +0800

    rs

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from mobo.algorithms import get_algorithm
 from visualization.data_export import DataExport
 from utils import save_args
 from ref_point import RefPoint
-# import torch
+import torch
 import gc
 
 import wandb
@@ -42,7 +42,7 @@ def run_experiment(args, framework_args):
 
     # set seed
     np.random.seed(args.seed)
-    # torch.manual_seed(args.seed)
+    torch.manual_seed(args.seed)
 
     # build problem, get initial samples
     problem, true_pfront, X_init, Y_init, rho_init = build_problem(
