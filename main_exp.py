faf8c207335819b73ba70eba72b42fce5c12b7f4
Karim Ben Hicham <karim.ben.hicham@rwth-aachen.de>
2024-03-03 20:35:27 +0800

    b5

--- main_exp.py
+++ main_exp.py
@@ -190,7 +190,7 @@ if __name__ == "__main__":
     
     args.algo = algo_name
     args.problem = 'exp4d'
-    args.n_iter = 5
+    args.n_iter = 6
     args.n_init_sample = 12
     args.batch_size = 2
     args.subfolder = 'optimization_4'
