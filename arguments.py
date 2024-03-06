43dbf773d7d0d28681fc64a9f40f33153412934c
Karim Ben Hicham <karim.ben.hicham@rwth-aachen.de>
2024-03-06 20:07:48 +0800

    20 iters

--- arguments.py
+++ arguments.py
@@ -9,9 +9,9 @@ Here we speficy different argument parsers to avoid argument conflict between in
 '''
 
 alpha = 0.9
-n_init_sample = 12
-n_iter = 5
-batch_size = 6
+n_init_sample = 6
+n_iter = 20
+batch_size = 1
 n_w = 11
  
 
